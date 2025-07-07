baa70baa784e0eae73a70ca3064707185923057e
alexjoel42 <alexcopperman1@gmail.com>
2025-07-07 00:15:12 -0400

    Fixing a small thing with index.hmtl where I wasn't doing a meta thing

--- ReadingCheck/dev-dist/sw.js
+++ ReadingCheck/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f001acab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3acjtmlu35g"
+    "revision": "0.9g82dj0upoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
