55a9b61a8b940150d5262c1457ed395342242751
alexjoel42 <alexcopperman1@gmail.com>
2025-08-01 17:37:55 -0400

    We are working to make the phrases better and more in line with standard practices. Next step is going to be letting users decide level and improving analytics

--- ReadingCheck/src/constants/phrases.ts
+++ ReadingCheck/src/constants/phrases.ts
@@ -1,10 +1,10 @@
 // src/constants/phrases.ts
-
+// wholesome, affirmational phrases mapped to K-2 phonics buckets
 export type Phrase = {
   id: string;
   text: string;
   sightWords: string[];
-  phoneticPatterns: string[];
+  phoneticPatterns: string[]; // O-G breakdown
 };
 
 export type PhraseSet = {
@@ -13,32 +13,51 @@ export type PhraseSet = {
   phrases: Phrase[];
 };
 
+// ---------- Kindergarten ----------
+const kPhrases: Phrase[] = [
+  { id: 'k01', text: "I am kind",             sightWords: ["I", "am"],           phoneticPatterns: ["ī", "ă-m", "k-ī-n-d"] },
+  { id: 'k02', text: "We can hug",            sightWords: ["we", "can"],         phoneticPatterns: ["w-ē", "k-ă-n", "h-ŭ-g"] },
+  { id: 'k03', text: "A big red dog",         sightWords: ["a", "big"],          phoneticPatterns: ["ă", "b-ĭ-g", "r-ĕ-d", "d-ŏ-g"] },
+  { id: 'k04', text: "The sun is up",         sightWords: ["the", "is"],         phoneticPatterns: ["th-ē", "s-ŭ-n", "ĭ-z", "ŭ-p"] },
+  { id: 'k05', text: "Let us sing",           sightWords: ["let", "us"],         phoneticPatterns: ["l-ĕ-t", "ŭ-s", "s-ĭ-ng"] },
+  { id: 'k06', text: "I see the ship",        sightWords: ["I", "see", "the"],   phoneticPatterns: ["ī", "s-ē", "th-ē", "sh-ĭ-p"] },
+  { id: 'k07', text: "She has a cat",        sightWords: ["she", "has", "a"],   phoneticPatterns: ["sh-ē", "h-ă-z", "ă", "c-ă-t"] },
+  { id: 'k08', text: "Pick up the rock",      sightWords: ["pick", "up", "the"], phoneticPatterns: ["p-ĭ-ck", "ŭ-p", "th-ē", "r-ŏ-ck"] },
+  { id: 'k09', text: "Stop and help",         sightWords: ["stop", "and", "help"], phoneticPatterns: ["s-t-ŏ-p", "ă-n-d", "h-ĕ-lp"] },
+  { id: 'k10', text: "Plan a kind act",       sightWords: ["plan", "a", "kind"], phoneticPatterns: ["p-l-ă-n", "ă", "k-ī-n-d", "ă-ct"] },
+];
+
+// ---------- Grade 1 ----------
+const g1Phrases: Phrase[] = [
+  { id: 'g101', text: "Make a safe space",          sightWords: ["make", "a"],        phoneticPatterns: ["m-ā-k", "ă", "s-ā-f", "sp-ā-ce"] },
+  { id: 'g102', text: "Ride the nice wave",         sightWords: ["ride", "the"],      phoneticPatterns: ["r-ī-d", "th-ē", "n-ī-s", "w-ā-v"] },
+  { id: 'g103', text: "Hope grows inside",          sightWords: ["hope", "grows"],    phoneticPatterns: ["h-ō-p", "gr-ō-z", "ĭ-n-s-ī-d"] },
+  { id: 'g104', text: "Use your wise mind",         sightWords: ["use", "your"],      phoneticPatterns: ["y-oo-z", "y-or", "w-ī-z", "m-ī-n-d"] },
+  { id: 'g105', text: "Share your kind heart",      sightWords: ["share", "your"],    phoneticPatterns: ["sh-air", "y-or", "k-ī-n-d", "h-ar-t"] },
+  { id: 'g106', text: "The star lights our path",   sightWords: ["the", "our"],       phoneticPatterns: ["th-ē", "st-ar", "l-ī-t-s", "ow-r", "p-ă-th"] },
+  { id: 'g107', text: "Her smile warms the room",   sightWords: ["her", "the"],       phoneticPatterns: ["h-er", "sm-ī-l", "w-or-m-z", "th-ē", "r-oo-m"] },
+  { id: 'g108', text: "Turn the page and learn",    sightWords: ["turn", "the", "and"], phoneticPatterns: ["t-er-n", "th-ē", "p-ā-j", "ă-n-d", "l-er-n"] },
+  { id: 'g109', text: "Keep your dreams alive",     sightWords: ["keep", "your"],     phoneticPatterns: ["k-ē-p", "y-or", "dr-ē-m-z", "ă-l-ī-v"] },
+  { id: 'g110', text: "We rise when we fall",       sightWords: ["we", "when"],       phoneticPatterns: ["w-ē", "r-ī-z", "w-eh-n", "w-ē", "f-aw-l"] },
+];
+
+// ---------- Grade 2 ----------
+const g2Phrases: Phrase[] = [
+  { id: 'g201', text: "Our joy will never end",         sightWords: ["our", "will", "never"],  phoneticPatterns: ["ow-r", "j-oi", "w-ih-l", "n-eh-v-er", "e-n-d"] },
+  { id: 'g202', text: "Clouds float above the town",    sightWords: ["above", "the"],            phoneticPatterns: ["cl-ow-d-z", "fl-ō-t", "ă-b-uh-v", "th-ē", "t-ow-n"] },
+  { id: 'g203', text: "The moon glows for us all",      sightWords: ["for", "us", "all"],        phoneticPatterns: ["th-ē", "m-oo-n", "gl-ō-z", "f-or", "ŭ-s", "aw-l"] },
+  { id: 'g204', text: "Few things feel better",         sightWords: ["few", "things"],           phoneticPatterns: ["f-ew", "th-ih-ng-z", "f-ē-l", "b-eh-t-er"] },
+  { id: 'g205', text: "Draw the best picture",          sightWords: ["draw", "the", "best"],     phoneticPatterns: ["dr-aw", "th-ē", "b-eh-s-t", "p-ih-k-ch-er"] },
+  { id: 'g206', text: "We painted joyful scenes",       sightWords: ["we", "joyful"],            phoneticPatterns: ["w-ē", "p-ā-n-t-e-d", "j-oi-f-uh-l", "s-ē-n-z"] },
+  { id: 'g207', text: "Running toward our dreams",      sightWords: ["toward", "our"],           phoneticPatterns: ["r-uh-n-ih-ng", "t-or-d", "ow-r", "dr-ē-m-z"] },
+  { id: 'g208', text: "Sunset brings warm light",       sightWords: ["brings"],                  phoneticPatterns: ["s-uh-n-s-eh-t", "br-ih-ng-z", "w-or-m", "l-ī-t"] },
+  { id: 'g209', text: "Inside every storm is calm",     sightWords: ["every", "storm"],          phoneticPatterns: ["ĭ-n-s-ī-d", "e-v-er-ē", "st-or-m", "ĭ-z", "k-ah-m"] },
+  { id: 'g210', text: "We learn and grow stronger",     sightWords: ["learn", "and"],            phoneticPatterns: ["w-ē", "l-er-n", "ă-n-d", "gr-ō", "str-or-ng-er"] },
+];
+
+// ---------- export to app ----------
 export const PHRASE_SETS = [
-  {
-    id: 'set-1',
-    focus: "CVC Words & Basic Sight Words",
-    phrases: [
-      { id: 'p1-1', text: "I am strong", sightWords: ["I", "am"], phoneticPatterns: ["I", "a-m", "s-t-r-aw-ng"] },
-      { id: 'p1-2', text: "We can win", sightWords: ["we", "can"], phoneticPatterns: ["w-ee", "k-ah-n", "w-ih-n"] },
-      { id: 'p1-3', text: "You are kind", sightWords: ["you", "are"], phoneticPatterns: ["y-oo", "ar", "k-ie-n-d"] },
-      { id: 'p1-4', text: "My joy grows", sightWords: ["my"], phoneticPatterns: ["m-y", "j-oi", "g-r-ow-s"] },
-      { id: 'p1-5', text: "Be your best", sightWords: ["be", "your"], phoneticPatterns: ["b-ee", "y-or", "b-eh-s-t"] },
-      { id: 'p1-6', text: "Love yourself", sightWords: ["love"], phoneticPatterns: ["l-uh-v", "y-or-s-eh-l-f"] },
-      { id: 'p1-7', text: "We help others", sightWords: ["we"], phoneticPatterns: ["w-ee", "h-eh-l-p", "uh-th-er-z"] },
-      { id: 'p1-8', text: "Try new things", sightWords: ["try"], phoneticPatterns: ["t-r-ie", "n-oo", "th-ih-ng-z"] },
-      { id: 'p1-9', text: "Dream big dreams", sightWords: ["dream"], phoneticPatterns: ["d-r-ee-m", "b-ih-g", "d-r-ee-m-z"] },
-      { id: 'p1-10', text: "Hope shines bright", sightWords: ["hope"], phoneticPatterns: ["h-ow-p", "sh-ie-n-z", "b-r-ie-t"] }
-    ]
-  },
-  {
-    id: 'set-2',
-    focus: "Digraphs & Blends",
-    phrases: [
-      { id: 'p2-1', text: "Share with friends", sightWords: ["share", "with"], phoneticPatterns: ["sh-air", "w-ih-th", "f-r-eh-n-d-z"] },
-      { id: 'p2-2', text: "Think happy thoughts", sightWords: ["think"], phoneticPatterns: ["th-ih-ng-k", "h-ae-p-ee", "th-aw-t-s"] },
-      { id: 'p2-3', text: "When the sun shines", sightWords: ["when", "the"], phoneticPatterns: ["w-eh-n", "th-ee", "s-uh-n", "sh-ie-n-z"] },
-      { id: 'p2-4', text: "Which path to choose", sightWords: ["which", "to"], phoneticPatterns: ["w-ih-ch", "p-ae-th", "t-oo", "ch-oo-z"] },
-      { id: 'p2-5', text: "Through thick and thin", sightWords: ["and"], phoneticPatterns: ["th-r-oo", "th-ih-k", "ae-n-d", "th-ih-n"] }
-    ]
-  }
-] satisfies readonly PhraseSet[];
+  { id: 'k-set',  focus: 'Kindergarten – CVC & Friends', phrases: kPhrases },
+  { id: 'g1-set', focus: 'Grade 1 – Magic e & Teams',    phrases: g1Phrases },
+  { id: 'g2-set', focus: 'Grade 2 – Diphthongs & Growth', phrases: g2Phrases },
+] as const;
